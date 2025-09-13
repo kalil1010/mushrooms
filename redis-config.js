c6c2878ce3f5a673bd8c049fefff439a8e94bdad
kalil1010 <224470782+kalil1010@users.noreply.github.com>
2025-09-13 18:11:51 +0300

    Update Redis configuration

--- redis-config.js
+++ redis-config.js
@@ -1,8 +1,6 @@
 const redis = require('redis');
-const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-});
+const url = process.env.REDIS_URL || 'redis://default:ibUgjVqIjtaOULMVcjcpFMYQHWzChaFo@redis.railway.internal:6379';
+const client = redis.createClient({ url });
 
 client.on('error', (err) => {
     console.error('Redis error:', err);
