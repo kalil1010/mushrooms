ecb0966f53ef7afbc72d7cc4997b0b2a2c796426
Kalil Jun <224470782+kalil1010@users.noreply.github.com>
2025-09-14 02:19:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const redis = require('./redis-config');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
